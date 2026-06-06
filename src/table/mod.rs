9a6978559a93859b2111300683466d7139801563
khammersmith17 <kilianhammersmith@gmail.com>
2026-06-06 13:51:46 -0400

    making drift entry pub

--- src/table/mod.rs
+++ src/table/mod.rs
@@ -1,7 +1,7 @@
 pub mod candidate;
 pub(crate) mod schema_view;
 pub use schema_view::InvalidSchemaReport;
-pub(crate) mod candidate_drift;
+pub mod candidate_drift;
 pub(crate) mod datatypes;
 pub(crate) mod slice_impl;
 pub use datatypes::DriftDataType;
